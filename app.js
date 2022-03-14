0ea54fd2c0cbd4e941bece94844372fb79df789e
hwkcode <hwkcode@gmail.com>
2022-03-14 09:10:46 -0400

    Add skeleton

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5500;
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
