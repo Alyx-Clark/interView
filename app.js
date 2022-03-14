50020cb554e8cf895bce5a194df66f5bcf1ab68c
hwkcode <hwkcode@gmail.com>
2022-03-14 19:31:00 -0400

    Add models and routes for user auth and videos

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const videos = require("./routes/api/videos");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,9 +17,13 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (req, res) => res.send("Hello World"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+app.use("/api/videos", videos);
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,89 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({                                                                                      // returns the user in the authentication route 
+        id: req.user.id,
+        email: req.user.email
+    });
+})
+
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    // Check to make sure nobody has already registered with a duplicate email
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                // Throw a 400 error if the email address already exists
+                return res.status(400).json({ email: "A user has already registered with this address" })
+            } else {
+                // Otherwise create a new user
+                const newUser = new User({
+                    fullname: req.body.fullname,
+                    email: req.body.email,
+                    password: req.body.password,
+                    industry: req.body.industry,
+                    experience: req.body.experience
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+});
+
+router.post("/login", (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            errors.email = "This user does not exist";
+            return res.status(400).json(errors);
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = { id: user.id, email: user.email };
+
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: "Bearer " + token
+                    });
+                });
+            } else {
+                errors.password = "Incorrect password";
+                return res.status(400).json(errors);
+            }
+        });
+    });
+});
 
 module.exports = router;
