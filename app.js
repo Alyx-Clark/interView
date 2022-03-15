4313e1087614180e0a1f2de1ad08afe6c8c9c132
Alyx-Clark <alexclark.edu@gmail.com>
2022-03-14 22:20:54 -0400

    set up s3

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const multer = require('multer') //
+const upload = multer({ dest: 'images/'}) //
+const fs = require('fs')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/images', express.static('images'))
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -16,6 +20,13 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+// app.post('/api/images', upload.single('image'), (req, res) => { 
+//     const imagePath = req.file.path 
+//     const description = req.body.description
+//     res.send({description, imagePath}) 
+    
+// }) 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
---
+++ s3.js
@@ -0,0 +1,40 @@
+const S3 = require('aws-sdk/clients/s3')
+require('dotenv').config()
+const fs = require('fs');
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({region, accessKeyId, secretAccessKey})
+
+
+
+
+
+
+function uploadFile(file) {
+  const fileStream = fs.createReadStream(file.path)
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename
+  }
+
+  return s3.upload(uploadParams).promise()
+}
+
+exports.uploadFile = uploadFile;
+
+function getFileStream(fileKey) {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
+  }
+
+  return s3.getObject(downloadParams).createReadStream()
+}
+
+exports.getFileStream = getFileStream
